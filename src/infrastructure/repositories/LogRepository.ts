5d1c886e7e442ecac91d4576882510859108f155
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-07-15 10:20:19 -0300

    bugfix: logErrorComposite data field

--- src/infrastructure/repositories/LogRepository.ts
+++ src/infrastructure/repositories/LogRepository.ts
@@ -5,7 +5,7 @@ export class LogRepository implements ILogErrorRepository {
   async logError (stack: string): Promise<void> {
     await prismaClient.errors.create({
       data: {
-        data: stack,
+        data: stack.substring(0, 191),
         date: new Date()
       }
     })
